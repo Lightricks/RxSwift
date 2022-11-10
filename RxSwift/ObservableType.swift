f9d747b8e430adec6f17b68912f4b6763b0ff2e9
Joe Muers <joe@lightricks.com>
2022-11-10 08:15:32 +0000

    SwiftLint: Resolve 2 violations, no logic changes.

--- RxSwift/ObservableType.swift
+++ RxSwift/ObservableType.swift
@@ -39,7 +39,7 @@ extension ObservableType {
     /// Default implementation of converting `ObservableType` to `Observable`.
     public func asObservable() -> Observable<Element> {
         // temporary workaround
-        //return Observable.create(subscribe: self.subscribe)
+        // return Observable.create(subscribe: self.subscribe)
         Observable.create { o in self.subscribe(o) }
     }
 }
